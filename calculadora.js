b4aa73454e603a6e2713fc080d18d5047b099933
Cesaredmyt <124023499+Cesaredmyt@users.noreply.github.com>
2025-03-11 09:44:53 -0600

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -4,8 +4,18 @@ function agregarNumero(num) {
 
 function agregarOperador(op) {
     let pantalla = document.getElementById("pantalla").value;
+    
     if (pantalla.length > 0 && !isNaN(pantalla[pantalla.length - 1])) {
         document.getElementById("pantalla").value += op;
+    }
+}
+
+function agregarDecimal() {
+    let pantalla = document.getElementById("pantalla").value;
+    let partes = pantalla.split(/[\+\-\*\/]/);
+
+    if (!partes[partes.length - 1].includes('.')) {
+        document.getElementById("pantalla").value += '.';
     }
 }
 
@@ -15,7 +25,10 @@ function limpiar() {
 
 function calcular() {
     try {
-        document.getElementById("pantalla").value = eval(document.getElementById("pantalla").value);
+        let resultado = eval(document.getElementById("pantalla").value);
+        
+        // Redondear si es decimal
+        document.getElementById("pantalla").value = Number.isInteger(resultado) ? resultado : resultado.toFixed(4);
     } catch {
         document.getElementById("pantalla").value = "Error";
     }
